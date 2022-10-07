7fae3e19b4939c961dd6eb2353f0ba27b548a623
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-10-07 23:39:10 +0300

    solution task 1 added

--- task_1/data_aggregation.py
+++ task_1/data_aggregation.py
@@ -18,7 +18,7 @@ def process_data(csv_file_path: str) -> json:
 
         for i in range(len(countries_arr)):
             tmp_about = {"people": [], "count": 0}
-            for j in range(len(file_arr)):
+            for j in range(1, len(file_arr)):
                 if file_arr[j][0] == countries_arr[i]:
                     tmp_about["people"].append(file_arr[j][1])
                     tmp_about["count"] = len(tmp_about["people"])
